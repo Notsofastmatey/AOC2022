3cf58326b323edc73f4f31f1dcbb6dc307387449
Notsofastmatey <mrsimonlaycock@gmail.com>
2022-12-12 21:39:14 +0000

    Name changes

--- Day10.py
+++ Day10.py
@@ -1,16 +1,16 @@
 class Computer:
-  def __init__(self, lines):
-    self.commands = lines
+  def __init__(self, commands):
+    self.commands = commands
     self.clock = 0
     self.x = 1
     self.totalSignalStrength = 0
 
   def run(self):
-    for line in self.commands:
-      if line[:4]=='noop':
+    for command in self.commands:
+      if command[:4]=='noop':
         self.noop()
       else:
-        v=int(line.split(' ')[1])
+        v=int(command.split(' ')[1])
         self.add(v)
 
   def noop(self):
@@ -28,7 +28,7 @@ class Computer:
       print(f"Clock: {self.clock} | X: {self.x} | Total Signal Strength: {self.totalSignalStrength}")
 
 def getData(fileName):
-  '''Gets the commands as a 2D list from the text file'''
+  '''Gets the commands from the text file'''
   with open(fileName, 'r') as f:
     data = f.readlines()   
   return data
